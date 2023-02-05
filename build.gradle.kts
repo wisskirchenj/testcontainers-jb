f2f3f6eca12dac19f96af0d3794f5a54f9eeaa2a
wisskirchenj <juergen.wisskirchen@cofinpro.de>
2023-02-05 18:43:17 +0100

    Gradlelize testcontainers-demo and bump to current libs

---
+++ build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.0.2"
+    id("io.spring.dependency-management") version "1.1.0"
+}
+
+group = "de.cofinpro"
+version = "0.5-SNAPSHOT"
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.mysql:mysql-connector-j:8.0.32")
+
+    implementation("org.seleniumhq.selenium:selenium-firefox-driver:4.8.0")
+    implementation("org.seleniumhq.selenium:selenium-remote-driver:4.8.0")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.named<Test>("test") {
+    useJUnitPlatform()
+}
